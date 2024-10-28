c9f46cc7c224a0e6df419407dad5c362b72eab3c
marcSantolayaSanchez <146827090+marcSantolayaSanchez@users.noreply.github.com>
2024-10-28 15:30:36 +0100

    AHORA SE COMPARA LA FIGURA HARD CODE CON LA ALEATORIA, BASICAMENTE CONTRA LA MAQUINA

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 MOVIMIENTO = 0
 
+def on_button_pressed_a():
+    basic.show_leds("""
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def on_gesture_shake():
     global MOVIMIENTO
     MOVIMIENTO = randint(1, 3)
     if MOVIMIENTO == 1:
-        basic.show_icon(IconNames.SCISSORS)
+        pass
     if MOVIMIENTO == 2:
         basic.show_leds("""
             . # # # .
@@ -22,3 +32,17 @@ def on_gesture_shake():
             # # # # #
             """)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_button_pressed_ab():
+    basic.show_icon(IconNames.SCISSORS)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.show_leds("""
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        """)
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let MOVIMIENTO = 0
+let MovimientoMaquina = 0
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         # # # # #
@@ -11,7 +12,7 @@ input.onButtonPressed(Button.A, function () {
 input.onGesture(Gesture.Shake, function () {
     MOVIMIENTO = randint(1, 3)
     if (MOVIMIENTO == 1) {
-    	
+        basic.showIcon(IconNames.Scissors)
     }
     if (MOVIMIENTO == 2) {
         basic.showLeds(`
@@ -30,6 +31,51 @@ input.onGesture(Gesture.Shake, function () {
             # # # # #
             # # # # #
             `)
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    MovimientoMaquina = 3
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    MovimientoMaquina = randint(1, 3)
+    if (MovimientoMaquina == 1) {
+        basic.showIcon(IconNames.Scissors)
+    }
+    if (MovimientoMaquina == 2) {
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    }
+    if (MovimientoMaquina == 3) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (MovimientoMaquina == 3) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . # . . .
+            . # # # .
+            `)
+    } else if (MovimientoMaquina > 2) {
+        basic.showIcon(IconNames.Angry)
+    } else {
+        basic.showIcon(IconNames.Happy)
     }
 })
 input.onButtonPressed(Button.AB, function () {
